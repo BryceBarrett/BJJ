d0d9b4010401c4fd2b275e658a805d402e24ec63
jalipala <alipalajb@g.cofc.edu>
2018-04-02 18:38:19 -0400

    commit test

---
+++ BJJAlarmClock/src/com/csci360/alarmclock/TimerTest/TimerTesting.java
@@ -0,0 +1,39 @@
+/*
+ * Just messing around with timers in java
+ */
+package timertesting;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+
+/**
+ *
+ * @author brycebarrett
+ */
+public class TimerTesting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        //Creating new timer object
+        Timer testTimer = new Timer();
+        TimerTask timeTest = new KeepingTime(11, 59, 40, "PM", false);
+        
+        //Creating the thread to run the "clock" in
+        testTimer.scheduleAtFixedRate(timeTest,0,1000);
+        
+//        /*
+//        *little while loop to keep printing the 
+//        */
+//        while(1==1){
+//            System.out.println(timeTest);
+//        }
+        
+       
+        //System.out.print(testTimer);
+    }
+    
+}
